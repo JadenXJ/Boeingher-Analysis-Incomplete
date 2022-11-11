bf36fe5ed6c4c85dcb7c44e6fc552765a39713e9
JadenXJ <86621543+JadenXJ@users.noreply.github.com>
2022-11-10 19:16:54 -0500

    Add files via upload

--- BoeingOrder&Delivery.sql
+++ BoeingOrder&Delivery.sql
@@ -1,83 +1,118 @@
+use BoeingOrderDelivery
+
 SELECT * 
-From [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries];
+From dbo.BoeingHistOrdersDeliveries
+WHERE Order_Year = 'All'
 
 --How many countries did Boeing service?
 
 SELECT COUNT (Distinct(Country)) AS #ofCountries
-From [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries];
+From dbo.BoeingHistOrdersDeliveries;
 
 --What customer ordered the most at a given time and when was that?
 
-SELECT [Customer Name], [Order Total], [Order Year], [Order Month]
-FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries]
-WHERE [Order Total] = (SELECT MAX([Order Total]) 
-					 FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries])
+--1st Way
+SELECT Customer_Name, Order_Total, Order_Year, Order_Month
+FROM dbo.BoeingHistOrdersDeliveries
+WHERE Order_Total = (SELECT MAX(Order_Total) 
+					 FROM dbo.BoeingHistOrdersDeliveries);
 
-SELECT [Customer Name], MAX([Order Total])
-FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries]
-GROUP BY [Customer Name]
-ORDER BY MAX([Order Total]) DESC
+--2nd Way
+SELECT Customer_Name, MAX(Order_Total) AS Highest_Order
+FROM dbo.BoeingHistOrdersDeliveries
+GROUP BY Customer_Name
+ORDER BY MAX(Order_Total) DESC
 	OFFSET 0 ROWS
-	FETCH NEXT 1 ROWS ONLY
+	FETCH NEXT 1 ROWS ONLY;
+
+SELECT Distinct(Customer_Name), Count(Order_Total) AS How_Many_Orders, MAX(Order_Total)
+FROM dbo.BoeingHistOrdersDeliveries
+GROUP BY Customer_Name;
+
 
 --How many of those orders went through for SpiceJet?
 
-SELECT [Customer Name], [Delivery Total], [Delivery Year ], [Unfilled Orders]
-FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries]
-WHERE [Customer Name] = (SELECT [Customer Name]
-							FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries]
-							GROUP BY [Customer Name]
-							ORDER BY MAX([Order Total]) DESC
+SELECT Customer_Name, Delivery_Total, Delivery_Year, Unfilled_Orders
+FROM dbo.BoeingHistOrdersDeliveries
+WHERE Customer_Name = (SELECT Customer_Name
+							FROM dbo.BoeingHistOrdersDeliveries
+							GROUP BY Customer_Name
+							ORDER BY MAX(Order_Total) DESC
 								OFFSET 0 ROWS
 								FETCH NEXT 1 ROWS ONLY)
 
 
 --What is the ratio of orders to delieveries?
-SELECT [Customer Name], [Order Year], [Order Month], [Delivery Year ], [Order Total]/[Delivery Total] AS DeliveryRatio
-FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries]
+SELECT Customer_Name, Order_Year, Order_Month, Delivery_Year, Order_Total/Delivery_Total AS DeliveryRatio
+FROM dbo.BoeingHistOrdersDeliveries
 
-SELECT [Customer Name], [Order Year], [Order Month], [Delivery Year ], (CAST([Order Year] AS int)-CAST([Delivery Year ] AS int) as DeliveryRatio
-FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries];
+--Doesn't work for characters, need to cast
+SELECT Customer_Name, Order_Year, Order_Month, Delivery_Year, CAST(Order_Year AS int) AS DeliveryRatio
+FROM dbo.BoeingHistOrdersDeliveries
+WHERE Order_Year != ALL(
+		SELECT TOP 2 Order_Year
+		From dbo.BoeingHistOrdersDeliveries
+		WHERE Order_Year = 'Dec' OR Order_Year = 'Nov'
+		GROUP BY Order_Year
+		)
+
+--Problem with cast because there are chars ALL route, delete that row.
+
+DELETE 
+From dbo.BoeingHistOrdersDeliveries
+WHERE Order_Year = 'All'
+
+SELECT Customer_Name, Order_Year, Order_Month, Delivery_Year, (CAST(Order_Year AS int)/CAST(Delivery_Year AS int)) AS DeliveryRatio
+FROM dbo.BoeingHistOrdersDeliveries
+WHERE Order_Year != ALL(
+		SELECT TOP 2 Order_Year
+		From dbo.BoeingHistOrdersDeliveries
+		WHERE Order_Year = 'Dec' OR Order_Year = 'Nov'
+		GROUP BY Order_Year
+		)
 
 --First I have to change the data type but that aint working. Ill comment it out
 --ALTER TABLE [BoeingHistOrder&Deliveries]
---ALTER COLUMN [Order Total] int
+--ALTER COLUMN Order_Total int
 
 --What is the time between the deliveries of orders?
-SELECT [Order Year], [Delivery Year ], [Order Year] - [Order Month] AS Delivery_Time
-FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries]
+SELECT Order_Year, Delivery_Year, Order_Year - Order_Month AS Delivery_Time
+FROM dbo.BoeingHistOrdersDeliveries
 
 --How many times did the company order from Boeing compared to others? Would include rank, but it is not working as well I would like.
---I would also like it to be a view but that is not working the way I want too. I'll comment it out for now.
 
---CREATE VIEW ReturningCustomerTable AS
-SELECT Distinct([Customer Name]), COUNT([CUSTOMER NAME]) AS ReturningCustomer--,RANK() (OVER ReturningCustomer ORDER BY Returning Customer)
-FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries]
-Group By [Customer Name]
+GO
+CREATE VIEW 
+dbo.ReturningCustomerTable AS
+SELECT Distinct(Customer_Name), COUNT(Customer_Name) AS ReturningCustomer--,RANK() (OVER ReturningCustomer ORDER BY Returning Customer)
+FROM dbo.BoeingHistOrdersDeliveries
+Group By Customer_Name
 ORDER BY ReturningCustomer DESC
+GO
 
-USE [BoeingOrder&Delivery]
 GO
 CREATE VIEW 
 dbo.Spice_Jet AS
-SELECT Distinct([Customer Name]), COUNT([CUSTOMER NAME]) AS ReturningCustomer
-FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries]
-WHERE [Customer Name]= 'SpiceJet'
-Group By [Customer Name];
+SELECT Distinct(Customer_Name), COUNT(Customer_Name) AS ReturningCustomer
+FROM dbo.BoeingHistOrdersDeliveries
+WHERE Customer_Name= 'SpiceJet'
+Group By Customer_Name;
 
+--DROP View Spice_Jet
+GO
 Select *
-FROM [BoeingOrder&Delivery]..Spice_Jet;
+FROM Spice_Jet;
 
 --Now I am curious about the engine and Model Series used?
 SELECT *
-FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries]
-WHERE [Customer Name] = 'SpiceJet'
+FROM dbo.BoeingHistOrdersDeliveries
+WHERE Customer_Name = 'SpiceJet'
 
 --So There are unfulfilled orders exist However they still Place orders. Order by time
 SELECT *
-FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries]
-WHERE [Customer Name] = 'SpiceJet'
-ORDER BY [Model Series]
+FROM dbo.BoeingHistOrdersDeliveries
+WHERE Customer_Name = 'SpiceJet'
+ORDER BY Model_Series
 
 --All Unfilled Orders are 737 Max Model Series and large quantites of them. This is odd business. ALso I am assuming there was at least
 --6 deliveries in 2013. Speaking of that maybe 2010 737-800 Model Series was not deliveried and Unfilled orders column was left unfilled.
@@ -85,6 +120,6 @@ ORDER BY [Model Series]
 --A few questions could be asked such as if there is a disportionate fullfillment rate between Model Series in general?
 --if there is a disporportionate fullfillment rate between countries and/or region? And most importantly why? I will investigate that
 
-SELECT [Model Series], [Delivery Total], [Order Total], [Unfilled Orders] COUNT([Model Series]) AS NumberOfModels
-FROM [BoeingOrder&Delivery]..[BoeingHistOrders&Deliveries]
-Group By [Model Series]
+SELECT Model_Series, Delivery_Total, Order_Total, Unfilled_Orders, COUNT(Model_Series) AS NumberOfModels
+FROM dbo.BoeingHistOrdersDeliveries
+Group By Model_Series
